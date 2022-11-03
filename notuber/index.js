115c9ce3c3dcfb7666623efb58bbf2daedc0c434
Kenny <knchua@ucdavis.edu>
2022-11-02 20:18:06 -0700

    initial commit
    Current position and marker works

--- notuber/index.js
+++ notuber/index.js
@@ -1,15 +1,35 @@
-let map;
+let map, infoWindow;
 
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
     const southstation = { lat: 42.352271, lng: -71.05524200000001 };
-
-    // The map, centered at Southstation
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 14,
+    map = new google.maps.Map(document.getElementById("map"), {
         center: southstation,
+        zoom: 14,
     });
+
+    infoWindow = new google.maps.InfoWindow();
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                map.setCenter(pos);
+            },
+            () => {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+
 
     const icons = {
         car: {
@@ -21,26 +41,7 @@ function initMap() {
             position: new google.maps.LatLng(42.3453, - 71.0464),
             type: "car",
         },
-        {
-            position: new google.maps.LatLng(42.3662, - 71.0621),
-            type: "car",
-        },
-        {
-            position: new google.maps.LatLng(42.3603, - 71.0547),
-            type: "car",
-        },
-        {
-            position: new google.maps.LatLng(42.3472, - 71.0802),
-            type: "car",
-        },
-        {
-            position: new google.maps.LatLng(42.3663, - 71.0544),
-            type: "car",
-        },
-        {
-            position: new google.maps.LatLng(42.3542, - 71.0704),
-            type: "car",
-        }
+
     ];
     // Create markers.
     for (let i = 0; i < features.length; i++) {
@@ -50,6 +51,37 @@ function initMap() {
             map: map,
         });
     }
+    // Adds current location marker
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            const marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+            })
+        },
+        () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+        }
+    );
+
+
+
+
+
+};
+// Error Handling
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 }
 
 window.initMap = initMap;
